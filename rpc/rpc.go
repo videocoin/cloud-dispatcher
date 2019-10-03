9453ecb483c208d326153eaaff8ac271d6af16e1
Nikita Savik <nikita@liveplanet.net>
2019-10-03 19:35:39 +0300

    Add new GetTask rpc method

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -3,17 +3,16 @@ package rpc
 import (
 	"context"
 
-	"github.com/mailru/dbr"
-
 	"github.com/jinzhu/copier"
+	"github.com/mailru/dbr"
 	v1 "github.com/videocoin/cloud-api/dispatcher/v1"
 	"github.com/videocoin/cloud-api/rpc"
 )
 
-func (s *RpcServer) GetTask(ctx context.Context, req *v1.TaskRequest) (*v1.Task, error) {
+func (s *RpcServer) GetPendingTask(ctx context.Context, req *v1.TaskPendingRequest) (*v1.Task, error) {
 	task, err := s.dm.GetPendingTask(ctx)
 	if err != nil {
-		logFailedTo(s.logger, "get task", err)
+		logFailedTo(s.logger, "get pending task", err)
 		return nil, rpc.ErrRpcInternal
 	}
 
@@ -26,6 +25,29 @@ func (s *RpcServer) GetTask(ctx context.Context, req *v1.TaskRequest) (*v1.Task,
 	if err != nil {
 		logFailedTo(s.logger, "mark as assigned", err)
 		return nil, rpc.ErrRpcInternal
+	}
+
+	v1Task := &v1.Task{}
+	err = copier.Copy(v1Task, task)
+	if err != nil {
+		logFailedTo(s.logger, "copy task", err)
+		return nil, rpc.ErrRpcInternal
+	}
+
+	v1Task.MachineID = task.MachineID.String
+
+	return v1Task, nil
+}
+
+func (s *RpcServer) GetTask(ctx context.Context, req *v1.TaskRequest) (*v1.Task, error) {
+	task, err := s.dm.GetTaskByID(ctx, req.ID)
+	if err != nil {
+		logFailedTo(s.logger, "get task", err)
+		return nil, rpc.ErrRpcInternal
+	}
+
+	if task == nil {
+		return nil, rpc.ErrRpcNotFound
 	}
 
 	v1Task := &v1.Task{}
