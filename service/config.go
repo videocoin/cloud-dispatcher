713c4fd039f76de4f10a3e37697e52e1734b6d8c
Nikita Savik <nikita@liveplanet.net>
2020-02-04 18:20:26 +0300

    fmt

--- service/config.go
+++ service/config.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Config struct {
-	Name    string `envconfig:"-"`
-	Version string `envconfig:"-"`
+	Name    string        `envconfig:"-"`
+	Version string        `envconfig:"-"`
+	Logger  *logrus.Entry `envconfig:"-"`
 
 	RPCAddr          string `default:"0.0.0.0:5008" envconfig:"RPC_ADDR"`
 	AccountsRPCAddr  string `default:"0.0.0.0:5001" envconfig:"ACCOUNTS_RPC_ADDR"`
@@ -18,15 +19,10 @@ type Config struct {
 	MinersRPCAddr    string `default:"0.0.0.0:5011" envconfig:"MINERS_RPC_ADDR"`
 	MetricsAddr      string `default:"0.0.0.0:15008" envconfig:"METRICS_ADDR"`
 	ConsulAddr       string `default:"127.0.0.1:8500" envconfig:"CONSUL_ADDR"`
-	Env				 string `default:"dev"`
-
-	DBURI string `default:"root:@/videocoin?charset=utf8&parseTime=True&loc=Local" envconfig:"DBURI"`
-	MQURI string `default:"amqp://guest:guest@127.0.0.1:5672" envconfig:"MQURI"`
-
-	AuthTokenSecret string `default:"" envconfig:"AUTH_TOKEN_SECRET"`
-
-	BaseInputURL  string `default:"" envconfig:"BASE_INPUT_URL"`
-	BaseOutputURL string `default:"" envconfig:"BASE_OUTPUT_URL"`
-
-	Logger *logrus.Entry `envconfig:"-"`
+	Env              string `default:"dev"`
+	BaseInputURL     string `default:"" envconfig:"BASE_INPUT_URL"`
+	BaseOutputURL    string `default:"" envconfig:"BASE_OUTPUT_URL"`
+	DBURI            string `default:"root:@/videocoin?charset=utf8&parseTime=True&loc=Local" envconfig:"DBURI"`
+	MQURI            string `default:"amqp://guest:guest@127.0.0.1:5672" envconfig:"MQURI"`
+	AuthTokenSecret  string `default:"" envconfig:"AUTH_TOKEN_SECRET"`
 }
