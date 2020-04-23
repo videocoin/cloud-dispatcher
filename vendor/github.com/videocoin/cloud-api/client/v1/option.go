216a20f91d67c7b4189c42f2792fbee1d020a13c
Aleksandr Didenko <aa.didenko@yandex.com>
2020-04-23 23:47:50 +0300

    Update deps

--- vendor/github.com/videocoin/cloud-api/client/v1/option.go
+++ vendor/github.com/videocoin/cloud-api/client/v1/option.go
@@ -5,9 +5,7 @@ import (
 	"time"
 
 	grpcmiddleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpczap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
-	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	grpcretry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	grpclogrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
 	grpctracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	grpcprometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/opentracing/opentracing-go"
@@ -22,47 +20,7 @@ func NewDefaultClientDialOption(ctx context.Context) []grpc.DialOption {
 			grpcmiddleware.ChainUnaryClient(
 				grpctracing.UnaryClientInterceptor(grpctracing.WithTracer(opentracing.GlobalTracer())),
 				grpcprometheus.UnaryClientInterceptor,
-				grpczap.UnaryClientInterceptor(ctxzap.Extract(ctx)),
-				grpcretry.UnaryClientInterceptor(
-					grpcretry.WithMax(3),
-					grpcretry.WithBackoff(grpcretry.BackoffLinear(500*time.Millisecond)),
-				),
-			),
-		),
-		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time:                time.Second * 10,
-			Timeout:             time.Second * 10,
-			PermitWithoutStream: true,
-		}),
-	}
-}
-
-func NewDefaultClientValidatorDialOption(ctx context.Context) []grpc.DialOption {
-	return []grpc.DialOption{
-		grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(
-			grpcmiddleware.ChainUnaryClient(
-				grpctracing.UnaryClientInterceptor(grpctracing.WithTracer(opentracing.GlobalTracer())),
-				grpcprometheus.UnaryClientInterceptor,
-				grpczap.UnaryClientInterceptor(ctxzap.Extract(ctx)),
-			),
-		),
-		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time:                time.Second * 10,
-			Timeout:             time.Second * 10,
-			PermitWithoutStream: true,
-		}),
-	}
-}
-
-func NewDefaultClientDialOptionWithoutRetry(ctx context.Context) []grpc.DialOption {
-	return []grpc.DialOption{
-		grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(
-			grpcmiddleware.ChainUnaryClient(
-				grpctracing.UnaryClientInterceptor(grpctracing.WithTracer(opentracing.GlobalTracer())),
-				grpcprometheus.UnaryClientInterceptor,
-				grpczap.UnaryClientInterceptor(ctxzap.Extract(ctx)),
+				grpclogrus.UnaryClientInterceptor(grpclogrus.Extract(ctx)),
 			),
 		),
 		grpc.WithKeepaliveParams(keepalive.ClientParameters{
