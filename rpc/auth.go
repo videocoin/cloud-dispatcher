5af6c151881e992df6e54fd9fbcf401d7ca67d17
Aleksandr Didenko <aa.didenko@yandex.com>
2020-04-24 21:19:46 +0300

    Add GetConfig rpc to exclude auth methods

--- rpc/auth.go
+++ rpc/auth.go
@@ -16,7 +16,7 @@ func nopAuth(ctx context.Context) (context.Context, error) {
 }
 
 func (s *Server) AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error) {
-	noAuthSuffixMethods := []string{"GetInternalConfig", "Health", "AddInputChunk"}
+	noAuthSuffixMethods := []string{"GetInternalConfig", "Health", "AddInputChunk", "GetConfig"}
 	for _, suffix := range noAuthSuffixMethods {
 		if strings.HasSuffix(fullMethodName, suffix) {
 			return ctx, nil
